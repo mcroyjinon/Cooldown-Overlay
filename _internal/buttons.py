d56566a9da583f866a3c0e034317f39628f7469d
SaekoHaato <123futureflash@gmail.com>
2025-06-08 03:51:52 -0700

    converted to exe file for ease of access

---
+++ _internal/buttons.py
@@ -0,0 +1,107 @@
+import tkinter as Tk
+import customtkinter as CTk
+from configparser import ConfigParser
+import win32gui
+import win32con
+
+
+class Input(CTk.CTk):
+
+    def __init__(self, key, type, text_var):
+        super().__init__()
+
+        self.geometry("300x100")
+        self.resizable(False, False)
+        self.title("Cooldown Overlay: Settings: Entry")
+        self.attributes("-topmost", True)
+        self.wm_attributes("-toolwindow", "true")
+
+        self.key = key
+        self.type = type
+        self.text_var = text_var
+
+        self.entry = CTk.CTkEntry(master=self, width=30)
+        self.entry.pack(side="top", pady=15, fill="x", padx=5)
+
+        enter = CTk.CTkButton(master=self, width=30, text="Enter", command=self.submit)
+        enter.pack(side="top")
+
+        self.mainloop()
+
+    def submit(self):
+        text = self.entry.get()
+
+        match self.type:
+            case "keybind":
+                if len(text) > 1:
+                    return
+            case "cooldown":
+                try:
+                    int(text)
+                except ValueError:
+                    return
+
+        previous_value = self.text_var.get()
+        self.text_var.set(text)
+
+        save_file = ConfigParser()
+        save_file.read("_internal/saves.ini")
+
+        match self.type:
+            case "keybind":
+                saved_value = save_file["keybinds"][previous_value]
+                save_file.remove_option("keybinds", previous_value)
+                save_file["keybinds"][text] = saved_value
+            case "name":
+                save_file["keybinds"][self.key] = text
+
+                saved_value = save_file["cooldowns"][previous_value]
+                save_file.remove_option("cooldowns", previous_value)
+                save_file["cooldowns"][text] = saved_value
+            case "cooldown":
+                save_file["cooldowns"][save_file["keybinds"][self.key]] = text
+
+        with open("_internal/saves.ini", "w") as file:
+            save_file.write(file)
+
+        hwnd = win32gui.FindWindow(None, "Cooldown Overlay: Settings")
+        if hwnd:
+            self.after(100, win32gui.PostMessage(hwnd, win32con.WM_CLOSE, 0, 0))
+
+        self.after(100, lambda: self.destroy())
+
+
+class ChangeableButtons(CTk.CTkButton):
+
+    def __init__(self, parent, key, type):
+
+        saves_file = ConfigParser()
+        saves_file.read("_internal/saves.ini")
+
+        text_var = CTk.StringVar(master=parent)
+
+        match type:
+            case "keybind":
+                text_var.set(key)
+            case "name":
+                text_var.set(saves_file["keybinds"][key])
+            case "cooldown":
+                text_var.set(saves_file["cooldowns"][saves_file["keybinds"][key]])
+
+        super().__init__(
+            parent,
+            height=30,
+            command=lambda: Input(key, type, text_var),
+            textvariable=text_var,
+            width=56,
+        )
+
+
+if __name__ == "__main__":
+    App = CTk.CTk()
+
+    button = ChangeableButtons(App, "1", "cooldown")
+
+    button.pack()
+
+    App.mainloop()
