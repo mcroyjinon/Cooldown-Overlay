0149baf999826e4e7426b0e80f687127228dddf9
SaekoHaato <123futureflash@gmail.com>
2025-06-20 08:02:06 -0700

    finished new settings screen

--- settings.py
+++ settings.py
@@ -1,6 +1,33 @@
 import customtkinter as CTk
 from keysettings import KeySettings
+from configparser import ConfigParser
 
+class BoolButtons(CTk.CTkButton):
+
+    def on_click(self) -> None:
+        self.configure(text= 'True' if not self.value else 'False')
+        self.value = not self.value
+
+        file: ConfigParser = ConfigParser()
+        file.read('saves.ini')
+
+        file['config'][self.setting] = str(self.value).lower()
+
+        with open('saves.ini', 'w') as content:
+            file.write(content)
+
+    def __init__(self, master, value: bool,setting: str):
+        super().__init__(
+            master=master,
+            text=str(value),
+            command=self.on_click,
+            width=2
+        )
+
+        self.value: bool = value
+        self.setting: str = setting
+
+        
 
 class Settings(CTk.CTk):
 
@@ -32,6 +59,31 @@ class Settings(CTk.CTk):
         keybind_button.place(x=215,y=130)
 
         #Settings
+        file: ConfigParser = ConfigParser()
+        file.read('saves.ini')
+
+        settings: dict[str: bool] = {key: (True if value == 'true' else False) for key, value in file['config'].items()}
+        print(settings)
+        
+        for setting, value in settings.items():
+            frame: CTk.CTkFrame = CTk.CTkFrame(main_frame, fg_color='transparent')
+
+            setting_label: CTk.CTkLabel = CTk.CTkLabel(
+                frame,
+                height=7,
+                width=5,
+                text=setting
+            )
+            setting_label.pack(side='left',expand=True)
+
+            value_button: BoolButtons = BoolButtons(
+                frame,
+                value,
+                setting
+            )
+            value_button.pack(side='right',expand=True)
+
+            frame.pack(side='top',pady=1,fill='x',padx=5)
 
         self.mainloop()
 
